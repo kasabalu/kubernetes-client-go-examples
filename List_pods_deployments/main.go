8a6335040a5a973c1c0a5ec2eef2ae2d2905f273
bkasa724 <balakrishna_kasa@comcast.com>
2022-06-07 12:28:46 -0400

    added deployment code

--- List_pods_deployments/main.go
+++ List_pods_deployments/main.go
@@ -21,7 +21,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	pods, err := clientset.CoreV1().Pods("default").List(context.Background(), metav1.ListOptions{})
+	ctx := context.Background()
+	pods, err := clientset.CoreV1().Pods("default").List(ctx, metav1.ListOptions{})
 
 	if err != nil {
 		fmt.Println(err)
@@ -29,6 +30,16 @@ func main() {
 	fmt.Println(pods.Items[0])
 	for _, pod := range pods.Items {
 		fmt.Println(pod.Name)
+	}
+
+	deployments, err := clientset.AppsV1().Deployments("minio-api").List(ctx, metav1.ListOptions{})
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("deployments are")
+	fmt.Println("")
+
+	for _, deployment := range deployments.Items {
+		fmt.Println(deployment.Name)
 	}
 
 }
