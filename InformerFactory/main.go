378748b6b5d8de09634aa6a630fa8a789d629288
bkasa724 <balakrishna_kasa@comcast.com>
2022-06-08 14:57:54 -0400

    deploy_client_go_app_in_k8s

---
+++ InformerFactory/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"time"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	kubeconfig := flag.String("Kubeconfig", "/Users/bkasa724/.kube/config", "location to kube config file")
+	namespace := flag.String("namespace", "default", "namespace name")
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	ns := *namespace
+	if err != nil {
+		fmt.Printf("error is %s building config from flags\n", err.Error())
+		// Inclusterconfig uses default SA which mounts on each to get the cluster config file.
+		config, err = rest.InClusterConfig()
+		// this will give authorization issues luke cannot list resource pods because pod uses default SA, that SA does not have  required permisions.
+		if err != nil {
+			fmt.Printf("error is %s building config from flags\n", err.Error())
+		}
+
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+	informerfactory := informers.NewSharedInformerFactory(clientset, 10*time.Minute)
+	podinformation := informerfactory.Core().V1().Pods()
+	podinformation.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(new interface{}) {
+			fmt.Println("Pod added and  trigger something")
+		},
+		UpdateFunc: func(old, new interface{}) {
+			fmt.Println("Updated trigger logic")
+
+		},
+		DeleteFunc: func(obj interface{}) {
+			fmt.Println("Updated trigger logic")
+		},
+	})
+
+	informerfactory.Start(wait.NeverStop)
+	informerfactory.WaitForCacheSync(wait.NeverStop)
+	pod, err := podinformation.Lister().Pods(ns).Get(ns)
+	fmt.Println(pod)
+}
